e746eea365e2e9d2fbd195264204757dff8856b6
karishmask46 <karishmask.15999@gmail.com>
2022-06-09 18:34:18 +0530

    [Karishma] check Employee present or not

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EmployeeWage
+{
+    class program
+    {
+       
+        public static void Main(string[] args)
+        {
+            UC1_EmployeePresent CheckEmp = new UC1_EmployeePresent();
+            CheckEmp.CheckEmpPresentAbsent();
+        }
+
+
+
+    }
+}
+
+
---
+++ EmployeeWage/EmployeeWage/UC1_EmployeePresent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class UC1_EmployeePresent
+    {
+        public int EmpPresent = 1;
+        public void CheckEmpPresentAbsent()
+        {
+            Random Empcheck = new Random();
+            int value = Empcheck.Next(0, 2);
+            if (value == EmpPresent)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
