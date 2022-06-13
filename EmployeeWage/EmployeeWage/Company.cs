3b85b1fb97bd282f2747ec0f084216e63ed5efed
karishmask46 <karishmask.15999@gmail.com>
2022-06-13 19:35:38 +0530

    [Karishma]Computing EmpWage For Multiple Companies

---
+++ EmployeeWage/EmployeeWage/Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Company
+    {
+        public float EmpWagePerHour;
+        public int Full_Time_WorkingHrs_perDay;
+        public int Part_Time_WorkingHrs_perDay;
+        public int MAX_WORKING_HRS;
+        public int MAX_WORKING_DAYS;
+
+
+        public string CompanyName;
+        public Company( string CompanyName, int EmpWagePerHour, int Full_Time_WorkingHrs_perDay, int Part_Time_WorkingHrs_perDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+        {
+            this.CompanyName = CompanyName;
+            this.EmpWagePerHour = EmpWagePerHour;
+            this.Full_Time_WorkingHrs_perDay = Full_Time_WorkingHrs_perDay;
+            this.Part_Time_WorkingHrs_perDay = Part_Time_WorkingHrs_perDay;
+            this.MAX_WORKING_HRS = MAX_WORKING_HRS;
+            this. MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+           
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/EmpWageComputation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpWageComputation
+    {
+        public const int EmpWagePerHour = 20;
+        public const int Full_Time_WorkingHrs_PerDay = 8;
+        public const int Part_Time_WorkingHrs_PerDay = 4;
+        public const int MAX_WORKING_HRS = 100;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int IS_ABSENT = 0;
+        float EmpDailyWage = 0;
+        private float TotalWage = 0;
+        public string CompanyName;
+       
+        
+
+                  
+       private Dictionary<string, Company> Companies = new Dictionary<string, Company>();
+
+       public void AddCompany(string CompanyName, int EmpWagePerHour, int Full_Time_WorkingHrs_PerDay, int Part_Time_WorkingHrs_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+        {
+            Company company = new Company(CompanyName.ToLower(), EmpWagePerHour, Full_Time_WorkingHrs_PerDay, Part_Time_WorkingHrs_PerDay, MAX_WORKING_HRS, MAX_WORKING_DAYS);
+            Companies.Add(CompanyName.ToLower(), company);
+        }
+        public  int IsEmployeePresent()
+        {
+            return new Random().Next() % 3;
+        }
+        public void CalucalteWage(string CompanyName)
+        {
+            Console.WriteLine(CompanyName + " company details");
+            int Daynumber = 1;
+            int EmpworkingHrs = 0;
+            int TotalWorkingHrs = 0;
+
+            if (!Companies.ContainsKey(CompanyName.ToLower()))         
+            throw new ArgumentNullException("company doesn't Exist");           
+            Companies.TryGetValue(CompanyName.ToLower(), out Company company);
+            while (Daynumber <= MAX_WORKING_DAYS && TotalWorkingHrs <= MAX_WORKING_HRS)
+            {
+                switch (IsEmployeePresent())
+                {
+                    case IS_ABSENT:
+                        EmpworkingHrs = 0;
+                        break;
+                    case IS_PART_TIME:
+                        EmpworkingHrs = Part_Time_WorkingHrs_PerDay;
+                        break;
+                    case IS_FULL_TIME:
+                        EmpworkingHrs = Full_Time_WorkingHrs_PerDay;
+                        break;
+                }
+                EmpDailyWage = EmpworkingHrs * EmpWagePerHour;
+
+                TotalWage += EmpDailyWage;
+                Daynumber++;
+                TotalWorkingHrs += EmpworkingHrs;
+
+
+            }
+            Console.WriteLine("Total Working days : " + MAX_WORKING_DAYS + "\nTotal Working hours : " + (TotalWorkingHrs) + "\nTotal Wage :" + (TotalWage));
+        }
+    }
+}
