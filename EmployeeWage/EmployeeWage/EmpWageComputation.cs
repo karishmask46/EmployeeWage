57fb905dee961a657945beb7ca1fcabb30284791
karishmask46 <karishmask.15999@gmail.com>
2022-06-16 20:18:13 +0530

    [Karishma]

--- EmployeeWage/EmployeeWage/EmpWageComputation.cs
+++ EmployeeWage/EmployeeWage/EmpWageComputation.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace EmployeeWage
 {
-    internal class EmpWageComputation
+    internal class EmpWageComputation : InterEmpWage
     {  
         public const int EmpWagePerHour = 20;
         public const int Full_Time_WorkingHrs_PerDay = 8;
@@ -24,19 +26,26 @@ namespace EmployeeWage
 
                   
        private Dictionary<string, Company> Companies = new Dictionary<string, Company>();
-        public string[] Companylist;
-        public int Arrayindex = 0;
+        //public string[] Companylist;
+        public ArrayList Companylist;
+       
+       // public int Arrayindex = 0;
+
         public EmpWageComputation(int number)
         {
-            Companylist = new string[2 * number];
+            // Companylist = new string[2 * number];
+            Companylist = new ArrayList(); 
+
         }
 
        public void AddCompany(string CompanyName, int EmpWagePerHour, int Full_Time_WorkingHrs_PerDay, int Part_Time_WorkingHrs_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
         {
             Company company = new Company(CompanyName.ToLower(), EmpWagePerHour, Full_Time_WorkingHrs_PerDay, Part_Time_WorkingHrs_PerDay, MAX_WORKING_HRS, MAX_WORKING_DAYS);
             Companies.Add(CompanyName.ToLower(), company);
-            Companylist[Arrayindex] = CompanyName;
-            Arrayindex++;
+            //Companylist[Arrayindex] = CompanyName;
+            //Arrayindex++;
+            Companylist.Add(CompanyName);
+            Companylist.Add(EmpWagePerHour * Full_Time_WorkingHrs_PerDay);
 
 
         }
@@ -50,6 +59,7 @@ namespace EmployeeWage
             int Daynumber = 1;
             int EmpworkingHrs = 0;
             int TotalWorkingHrs = 0;
+            int MonthlyWage = 0;
 
             if (!Companies.ContainsKey(CompanyName.ToLower()))         
             throw new ArgumentNullException("company doesn't Exist");           
@@ -73,17 +83,20 @@ namespace EmployeeWage
                 TotalWage += EmpDailyWage;
                 Daynumber++;
                 TotalWorkingHrs += EmpworkingHrs;
+                MonthlyWage += EmpworkingHrs;
 
 
             }
-            Companylist[Arrayindex] = Convert.ToString(TotalWorkingHrs);
-            Arrayindex++;
+            Companylist.Add(MonthlyWage);
+            //Companylist[Arrayindex] = Convert.ToString(TotalWorkingHrs);
+            // Arrayindex++;
+            Companylist.Add(MonthlyWage);
 
             Console.WriteLine("Total Working days : " + MAX_WORKING_DAYS + "\nTotal Working hours : " + (TotalWorkingHrs) + "\nTotal Wage :" + (TotalWage));
         }
         public void displayArray()
         {
-            for (int i = 0; i < Companylist.Length; i +=2)
+            for (int i = 0; i < Companylist.Count; i +=2)
             {
                 Console.WriteLine("Monthly Wage for {0} is {1}",Companylist[i], Companylist[i+1]);
             }
---
+++ EmployeeWage/EmployeeWage/InterEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal interface InterEmpWage
+    {
+        public void AddCompany(string CompanyName, int EmpWagePerHour, int Full_Time_WorkingHrs_perDay, int Part_Time_WorkingHrs_perDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS);
+        public void CalucalteWage(string CompanyName);
+    }
+}
